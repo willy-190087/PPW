9262055e4252e2e353e300769bfd6ced7a019424
wirasenacenter <wirasenacenter@gmail.com>
2022-06-30 21:42:56 +0700

    commit

--- _build/jupyter_execute/topic-modelling.py
+++ _build/jupyter_execute/topic-modelling.py
@@ -9,12 +9,20 @@
 # - OS
 # - Regex
 # - Pandas
+# - Matplotlib
 # - NLTK
 # - Sklearn
 # - Sastrawi
 # - Wordcloud
 
 # In[1]:
+
+
+get_ipython().system('pip install Sastrawi')
+get_ipython().system('pip install Scrapy')
+
+
+# In[2]:
 
 
 # Import Library
@@ -29,6 +37,8 @@ from sklearn.decomposition import LatentDirichletAllocation
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from wordcloud import WordCloud
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 # Install NLTK Corpus
 nltk.download('stopwords')
@@ -41,21 +51,21 @@ nltk.download('punkt')
 # 
 # Pada bagian ini digunakan untuk membuat proyek library Scrapy dan memindah posisi direktori. Proyek library Scrapy diberi nama crawlproject. Posisi direktori dipindah ke crawlproject/crawlproject/spiders
 
-# In[2]:
+# In[3]:
 
 
 # Membuat proyek library Scrapy
 get_ipython().system('scrapy startproject crawlproject')
 
 
-# In[3]:
+# In[4]:
 
 
 # Melihat posisi direktori saat ini
 os.getcwd()
 
 
-# In[4]:
+# In[5]:
 
 
 # Mengubah posisi direktori saat ini ke crawlproject/crawlproject/spiders
@@ -66,15 +76,15 @@ os.getcwd()
 
 # ## Crawling Link PTA
 # 
-# Pada bagian ini digunakan untuk membuat dan menjalankan program python. Program tersebut digunakan untuk melakukan _crawling_ 40 link tugas akhir teknik informatika. Untuk melakukan _crawling_ menggunakan library scrapy.
+# Pada bagian ini digunakan untuk membuat dan menjalankan program python. Program tersebut digunakan untuk melakukan _crawling_ 40 link tugas akhir teknik manajemen. Untuk melakukan _crawling_ menggunakan library scrapy.
 
-# In[5]:
+# In[6]:
 
 
 get_ipython().run_cell_magic('writefile', '-a link.py', '# Membuat file link.py\n# File link.py digunakan untuk crawling link tugas akhir\nimport scrapy\n\nclass QuotesSpider(scrapy.Spider):\n    name = "quotes"\n\n    def start_requests(self):\n        start_urls = [\'https://pta.trunojoyo.ac.id/c_search/byprod/10/1\']\n        for i in range (2,9):\n            tambah = \'https://pta.trunojoyo.ac.id/c_search/byprod/10/\'+ str(i)\n            start_urls.append(tambah)\n        for url in start_urls:\n            yield scrapy.Request(url=url, callback=self.parse)\n\n    def parse(self, response):\n        for i in range(1, 6):\n            yield {\n                \'link\':response.css(\'#content_journal > ul > li:nth-child(\' +str(i)+ \') > div:nth-child(3) > a::attr(href)\').extract()\n            }\n')
 
 
-# In[6]:
+# In[7]:
 
 
 # Menjalankan file link.py untuk melakukan proses crawling link tugas akhir
@@ -85,15 +95,15 @@ get_ipython().system('scrapy runspider link.py -o link.csv')
 
 # ## Crawling Detail PTA
 # 
-# Pada bagian ini digunakan untuk membuat dan menjalankan program python. Program tersebut digunakan untuk melakukan crawling 40 detail tugas akhir informatika. Untuk melakukan crawling menggunakan library scrapy.
+# Pada bagian ini digunakan untuk membuat dan menjalankan program python. Program tersebut digunakan untuk melakukan crawling 40 detail tugas akhir manajemen. Untuk melakukan crawling menggunakan library scrapy.
 
-# In[7]:
+# In[8]:
 
 
 get_ipython().run_cell_magic('writefile', '-a detail.py', '# Membuat file detail.py\n# File detail.py digunakan untuk crawling detail tugas akhir\nimport scrapy\nimport pandas as pd\n\nclass QuotesSpider(scrapy.Spider):\n    name = "quotes"\n\n    def start_requests(self):\n        dataCSV = pd.read_csv(\'link.csv\')\n        indexData = dataCSV.iloc[:, [0]].values\n        arrayData = []\n        for i in indexData:\n            ambil = i[0]\n            arrayData.append(ambil)\n        for url in arrayData:\n            yield scrapy.Request(url=url, callback=self.parse)\n\n    def parse(self, response):\n        yield {\n            \'judul\': response.css(\'#content_journal > ul > li > div:nth-child(2) > a::text\').extract(),\n            \'penulis\': response.css(\'#content_journal > ul > li > div:nth-child(2) > div:nth-child(2) > span::text\').extract(),\n            \'pembimbing_1\': response.css(\'#content_journal > ul > li > div:nth-child(2) > div:nth-child(3) > span::text\').extract(),\n            \'pembimbing_2\': response.css(\'#content_journal > ul > li > div:nth-child(2) > div:nth-child(4) > span::text\').extract(),\n            \'abstrak\': response.css(\'#content_journal > ul > li > div:nth-child(4) > div:nth-child(2) > p::text\').extract()\n        }\n')
 
 
-# In[8]:
+# In[9]:
 
 
 # Menjalankan file detail.py untuk melakukan proses crawling detail tugas akhir
@@ -108,7 +118,7 @@ get_ipython().system('scrapy runspider detail.py -o detail.csv')
 # 
 # Pada bagian ini digunakan untuk membaca dataset. Dataset akan dibaca dan diubah menjadi dataframe agar lebih mudah diolah. Hanya kolom abstrak yang diambil untuk diolah.
 
-# In[9]:
+# In[10]:
 
 
 # Membaca dataset dan hanya mengambil kolom
@@ -118,7 +128,7 @@ docs = pd.read_csv('detail.csv', usecols=['abstrak'])
 # ## Cleaning & Tokenizing
 # Pada bagian ini digunakan untuk membersihkan dan melakukan tokenisasi data yang sudah dicrawling. Fungsi dari membersihkan data adalah membuang tanda baca dan membuatnya menjadi _lowercase_. Kemudian dilakukan tokenisasi untuk memecah kata.
 
-# In[10]:
+# In[11]:
 
 
 # Membersihkan dan melakukan tokenisasi data yang sudah dicrawling
@@ -135,7 +145,7 @@ docs['abstrak']
 # 
 # <a href="https://github.com/stopwords-iso/stopwords-id/blob/master/raw/indonesian-stopwords-complete.txt" title="indonesian-stopwords-complete.txt">Kamus Stopword Bahasa Indonesia</a>
 
-# In[11]:
+# In[12]:
 
 
 # Menghapus kata-kata bahasa indonesia dan inggris yang termasuk stopword
@@ -155,25 +165,14 @@ docs['abstrak_no_sw']
 # ### Stemming Dictionary
 # <a href="https://github.com/har07/PySastrawi/blob/master/src/Sastrawi/Stemmer/data/kata-dasar.txt" title="kata-dasar.txt">Kamus Stemming Bahasa Indonesia</a>
 
-# In[12]:
+# In[13]:
 
 
 # Mengubah sebuah kata menjadi bentuk dasar dari kata tersebut
-term_dict = {}
 factory = StemmerFactory()
 stemmer = factory.create_stemmer()
 
-for doc in docs['abstrak_no_sw']:
-    for term in doc:
-        if term not in term_dict:
-            term_dict[term] = ' '
-
-for term in term_dict:
-    term_dict[term] = stemmer.stem(term)
-
-docs['abstrak_no_sw_stemmed'] = docs['abstrak_no_sw'].apply(
-    lambda doc: [term_dict[term] for term in doc])
-docs['deskripsi'] = docs['abstrak_no_sw_stemmed'].apply(lambda doc: " ".join([x for x in doc]))
+docs['deskripsi'] = docs['abstrak_no_sw'].apply(lambda doc: " ".join([x for x in doc]))
 docs['deskripsi']
 
 
@@ -207,7 +206,7 @@ docs['deskripsi']
 # tfidf( t,d,D ) = tf( t,d ) \times idf( t,D )
 # $$
 
-# In[13]:
+# In[14]:
 
 
 # Proses Term Frequency - Inverse Document Frequency
@@ -217,7 +216,7 @@ attr_count = vect.get_feature_names_out().shape[0]
 print(f'Jumlah term dalam kumpulan dokumen : {attr_count}')
 
 
-# In[14]:
+# In[15]:
 
 
 # Menyimpan hasil TF-IDF ke dalam DataFrame
@@ -228,7 +227,7 @@ tfidf = pd.DataFrame(
 tfidf.head()
 
 
-# In[15]:
+# In[16]:
 
 
 # Menampilkan 5 kata paling sering muncul
@@ -240,7 +239,7 @@ for i, word in enumerate(l[:5]):
     print(f"{i+1}. {word}\t(Nilai idf: {dd[word]})")
 
 
-# In[16]:
+# In[17]:
 
 
 # Menampilkan 5 kata paling jarang muncul
@@ -280,7 +279,7 @@ for i, word in enumerate(l[:-5:-1]):
 # 
 # Dalam mengimplementasikan LSA, dapat menggunakan fungsi TruncatedSVD. parameter n_components digunakan untuk menentukan jumlah topik yang akan diekstrak.
 
-# In[17]:
+# In[18]:
 
 
 # Melakukan Latent Semantic Analysis
@@ -291,7 +290,7 @@ print(f"Jumlah dokumen\t: {count_doc_lsa}")
 print(f"Jumlah topik\t: {count_topic_lsa}")
 
 
-# In[18]:
+# In[19]:
 
 
 # Komposisi dokumen 0 berdasar topik
@@ -302,7 +301,7 @@ for i,topic in enumerate(lsa_top[0]):
 
 # Dari hasil diatas dapat kita simpulkan bahwa Topic 2 lebih dominan daripada topik 0 pada document 0
 
-# In[19]:
+# In[20]:
 
 
 # Menampilkan jumlah topik dan term
@@ -313,7 +312,7 @@ print(f"Jumlah kata\t: {count_word}")
 
 # ### 10 term penting setiap topik
 
-# In[20]:
+# In[21]:
 
 
 # Term paling penting untuk setiap topik
@@ -359,7 +358,7 @@ for i, comp in enumerate(lsa_model.components_):
 # - max_iter = 1 \
 #     Untuk mengatur jumlah iterasi training data (epoch) menjadi 1 kali saja.
 
-# In[21]:
+# In[22]:
 
 
 # Melakukan Latent Dirichlet Allocation
@@ -370,7 +369,7 @@ print(f"Jumlah dokumen\t: {count_doc_lda}")
 print(f"Jumlah topik\t: {count_topic_lda}")
 
 
-# In[22]:
+# In[23]:
 
 
 # Komposisi dokumen 0 berdasar topik
@@ -381,7 +380,7 @@ for i,topic in enumerate(lda_top[0]):
 
 # Dari hasil diatas dapat kita simpulkan bahwa Topic 1 lebih dominan daripada topik 0 pada document 0
 
-# In[23]:
+# In[24]:
 
 
 # Menampilkan jumlah topik dan term
@@ -392,7 +391,7 @@ print(f"Jumlah Term\t: {count_word_lda}")
 
 # ### 10 term penting setiap topik
 
-# In[24]:
+# In[25]:
 
 
 # Mendapatkan term penting untuk setiap topik
@@ -411,7 +410,7 @@ for i, comp in enumerate(lda_model.components_):
 
 # ### 50 term penting dengan wordcloud
 
-# In[25]:
+# In[26]:
 
 
 # Membuat gambar word cloud setiap topik
@@ -426,16 +425,96 @@ def draw_word_cloud(index):
   plt.show()
 
 
-# In[26]:
+# In[27]:
 
 
 # Menampilkan hasil word cloud topik 1
 draw_word_cloud(0)
 
 
-# In[27]:
+# In[28]:
 
 
 # Menampilkan hasil word cloud topik 2
 draw_word_cloud(1)
+
+
+# # Clustering
+
+# ## Dimentional reduction PCA
+# Principal component analysis (PCA) adalah suatu teknik analisis statistik multivariat. Bisa dibilang, inilah teknik analisis statistik yang paling populer sekarang. Biasanya, PCA digunakan dalam bidang pengenalan pola serta pemrosesan sinyal. \
+# PCA pada dasarnya merupakan dasar dari analisis data multivariat yang menerapkan metode proyeksi. Teknik analisis ini biasanya digunakan untuk meringkas tabel data multivariat dalam skala besar hingga bisa dijadikan kumpulan variabel yang lebih kecil atau indeks ringkasan. Dari situ, kemudian variabel dianalisis untuk mengetahui tren tertentu, klaster variabel, hingga outlier.
+
+# In[29]:
+
+
+# melihat n_component terbaik
+# dengan cumulative explained variance
+pca = PCA().fit(tfidf)
+cmv = pca.explained_variance_ratio_.cumsum()
+print(cmv)
+print(cmv.shape)
+
+
+# In[30]:
+
+
+# proses reduksi dimensi
+pca = PCA(n_components=40)
+pca_df = pca.fit_transform(tfidf)
+pca_df
+
+
+# ## Metode Elbow
+# Metode Elbow merupakan salah satu metode untuk menentukan jumlah cluster yang tepat melalui persentase hasil perbandingan antara jumlah cluster yang akan membentuk siku pada suatu titik. Jika nilai cluster pertama dengan nilai cluster kedua memberikan sudut dalam grafik atau nilainya mengalami penurunan paling besar maka jumlah nilai cluster tersebut yang tepat. Untuk mendapatkan perbandingannya adalah dengan menghitung Sum of Square Error (SSE) dari masing-masing nilai cluster. Karena semakin besar jumlah nilai cluster K, maka nilai SSE akan semakin kecil. 
+# 
+# $$S S E=\sum_{K=1}^{K} \sum_{X_{i}}\left|x_{i}-c_{k}\right|^{2}$$
+# 
+# Keterangan:\
+# ${K}$ = _cluster_ ke-c\
+# $x_{i}$ = jarak data obyek ke-i\
+# $c_{k}$ = pusat _cluster_ ke-i
+
+# In[31]:
+
+
+elbow = []
+for i in range(1, 11):
+  kmeans = KMeans(n_clusters=i, random_state=5)
+  kmeans.fit(pca_df)
+  elbow.append(kmeans.inertia_)
+plt.plot(range(1, 11), elbow, 'bx-')
+plt.title('Metode Elbow')
+plt.xlabel('Jumlah cluster')
+plt.ylabel('elbow')
+plt.show()
+
+
+# ### Mencari _cluster_ terbaik menggunakan metode elbow tidak berhasil karena posisi elbow tidak terlihat. Hal ini dikarenakan minimnya term.kata unik yang terdapat pada tugas tugas akhir manajemen.
+
+# ## K-Means Clustering
+# K-Means Clustering merupakan algoritma yang efektif untuk menentukan cluster dalam sekumpulan data, di mana pada algortima tersebut dilakukan analisis kelompok yang mengacu pada pemartisian N objek ke dalam K kelompok (Cluster) berdasarkan nilai rata-rata (means) terdekat. Adapun persamaan yang sering digunakan dalam pemecahan masalah dalam menentukan jarak terdekat adalah persamaan Euclidean berikut :
+# 
+# $$d(p, q)=\sqrt{\left(p_{1}-q_{1}\right)^{2}+\left(p_{2}-q_{2}\right)^{2}+\left(p_{3}-q_{3}\right)^{2}}$$
+# Keterangan:\
+# _d_ = jarak obyek\
+# _p_ = data\
+# _q_ = centroid
+
+# In[32]:
+
+
+# melakukan clustering menggunakan kmeans
+# jumlah cluster yang digunakan adalah 2
+kmeans = KMeans(n_clusters=2, random_state=5)
+kmeans.fit(pca_df)
+y_kmeans = kmeans.predict(pca_df)
+y_kmeans
+
+
+# In[33]:
+
+
+# menampilkan hasil clustering kmeans clustering
+plt.scatter(pca_df[:, 0], pca_df[:, 8], c=y_kmeans)
 
